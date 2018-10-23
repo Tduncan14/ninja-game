eae14e7ded426886b5b5730f858469a9295c3093
Tre'Shawn Duncan <nbatre1@yahoo.com>
2018-10-22 21:02:20 -0500

    keep moving forward

--- javascript/javascripts.js
+++ javascript/javascripts.js
@@ -1,3 +1,11 @@
+/// Setting up the canvas for the website
+
+var canvas=document.createElement('canvas');
+canvas.width=800;
+canvas.height=600;
+document.body.appendChild(canvas);
+
+// coloring the canvas
 var canvass =canvas.getContext('2d');
 canvass.fillStyle='green';
 canvass.fillRect(10,10,30,30);
