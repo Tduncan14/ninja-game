029bb7b39f8a40980750f593fa706c2f859d1596
Tre'Shawn Duncan <nbatre1@yahoo.com>
2018-10-23 22:43:37 -0500

    Keep going strong

--- javascript/javascripts.js
+++ javascript/javascripts.js
@@ -1,4 +1,12 @@
 // Constants
+// width of the canvas
+const canvasWidth = 800;
+//height of of the canvas
+const canvasHeight = 600;
+//  The height of the character
+const nanonautWidth = 181;
+// The width of the character
+const groundY = 540;
 
 
 
