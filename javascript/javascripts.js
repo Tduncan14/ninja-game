1743f73d046fec29db455c96b700151eb6b7ae8b
Tre'Shawn Duncan <nbatre1@yahoo.com>
2018-10-23 22:59:20 -0500

    keep moving forward

--- javascript/javascripts.js
+++ javascript/javascripts.js
@@ -19,9 +19,26 @@ var c = canvas.getContext('2d');
 canvas.width = canvasWidth;
 // attaching to canvas height
 canvas.height = canvasHeight;
+// appending the created element to the child of the body
+document.body.appendChild(canvas);
 
+// adding the image with the image object
 
+var nanonautImage= new Image();
+// adding the source to it
+nanonautImage.src ='images/nanonaut.png';
+
+var nanonautX =50;
+var nanonautY=40;
+
+//Once the image is loaded run and call the function
+window.addEventListener('load', start);
+
+function start () {
+    window.requestAnimationFrame(mainLoop);
+}
 /// MainLOOP
+
 
 
 
